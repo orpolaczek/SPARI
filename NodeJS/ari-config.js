bbfbbab5789f0e665716edd55c6dd529f5c598de
Or Polaczek <or.polaczek@greefieldtech.net>
2016-07-01 23:31:36 +0300

    Adding create-user script

---
+++ NodeJS/ari-config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by orpolaczek on 10/10/2015.
+ */
+var ARICredentials = {
+    ariServer: 'astricon.demo.server:8088',
+    ariUsername: 'astricondemouser',
+    ariPassword: 'test4321',
+    ariUseHTTPS: false,
+    httpsValidateCert: false
+};
+
+module.exports = ARICredentials;
